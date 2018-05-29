e41e7ff0ed2f96a51e6e1fead044a97a4a7d4d3b
zoheezus <37160795+zoheezus@users.noreply.github.com>
2018-05-29 18:13:12 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -356,12 +356,14 @@
                 <form method="post" action="mailer.php" class="contact-form">
                     
                     <div class="row">
-                        <div class="form-messages success">
-                            Thank you! Your message has been sent.
-                        </div>
-                       <!-- <div class="form-messages error">
-                            Oops! Something went wrong. Please try again!
-                        </div> -->
+                        <?php
+                            if($_GET['success'] == 1) {
+                                echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>"
+                            }
+                            if($_GET['success'] == -1) {
+                                echo "<div class=\"form-messages error\">Oops! Something went wrong. Please try again!</div>"
+                            }
+                        ?>
                     </div>
                     
                     <div class="row">
